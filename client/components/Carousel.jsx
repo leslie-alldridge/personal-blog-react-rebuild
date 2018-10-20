9f36312b10cf2ee8c717c62736794d5ed8cbde61
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-21 10:43:01 +1300

    going with new cards

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  Thumbnail,
+  Button,
+  Row,
+  Col
+} from 'react-bootstrap';
 
 export default class extends React.Component {
   state = {
@@ -7,130 +19,39 @@ export default class extends React.Component {
   };
   render() {
     return (
-      <div style={{ width: '90%', margin: 'auto' }}>
-        <Carousel
-          edgeEasing={true}
-          autoplay={true}
-          autoplayInterval={7000}
-          heightMode={'max'}
-          initialSlideHeight={'20'}
-          slideIndex={this.state.slideIndex}
-          afterSlide={slideIndex => this.setState({ slideIndex })}
-        >
-          <div id="firstCar" class="tile">
-            <div class="tile is-parent">
-              <article class="tile is-child notification is-info">
-                <p class="title">BackPack Tracker</p>
-                <p class="subtitle">
-                  Keep track of your belongings (mobile first). A full stack
-                  application with authentication, internal API and databases.
-                </p>
-                <figure id="blogDetailImg" class="image is-16by9">
-                  <a
-                    href="https://backpack-tracker.herokuapp.com/"
-                    target="_blank"
-                  >
-                    <img id="round" src="/new.png" />
-                  </a>
-                </figure>
-              </article>
-            </div>
-          </div>
-          <div id="firstCar" class="tile">
-            <article class="tile is-child notification is-info">
-              <p class="title">GYST - Personal Dashboard</p>
-              <p class="subtitle">
+      <Grid style={{ display: 'flex' }}>
+        <Row>
+          <Col xs={6} md={4}>
+            <Thumbnail src="/new.png" alt="242x200">
+              <h3>BackPack Tracker - Track multiple bags with ease</h3>
+              <p>
+                Keep track of your belongings (mobile first). A full stack
+                application with authentication, internal API and databases.
+              </p>
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="/gyst.png" alt="242x200">
+              <h3>GYST - Custom Personal Dashboard</h3>
+              <p>
                 Final Group Project EDA. Fullstack with many API's (including
                 Google and Spotify). Material UI, React Flex Grid, D3 Charts,
                 live chat and more.
               </p>
-              <figure id="blogDetailImg" class="image is-16by9">
-                <a
-                  href="https://github.com/leslie-alldridge/GYST-Final-Project-EDA"
-                  target="_blank"
-                >
-                  <img id="round" src="/gyst.png" />
-                </a>
-              </figure>
-            </article>
-          </div>
-          <div id="firstCar" class="tile">
-            <article class="tile is-child notification is-info">
-              <p class="title">Xero Bulk Voiding Tool</p>
-              <p class="subtitle">
-                Void invoices in bulk using the Xero API. Inspired by a
-                community thread{' '}
-                <a
-                  target="_blank"
-                  href="https://community.xero.com/business/discussion/262531"
-                >
-                  here
-                </a>
-                . Currently building a second version in React.
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="/void2.png" alt="242x200">
+              <h3> Void invoices in bulk using the Xero API.</h3>
+              <p>
+                Inspired by a Xero Community feature request thread. Built using
+                express-handlebars early in my career, new version released
+                below.
               </p>
-              <figure id="blogDetailImg" class="image is-16by9">
-                <a
-                  href="https://github.com/leslie-alldridge/xero-bulk-void-invoice-tool-v1"
-                  target="_blank"
-                >
-                  <img id="round" src="/void2.png" />
-                </a>
-              </figure>
-            </article>
-          </div>
-          <div id="firstCar" class="tile">
-            <article class="tile is-child notification is-info">
-              <p class="title">Xero Invoice Reducer</p>
-              <p class="subtitle">
-                Create summarised invoices with ease. This can help customers
-                who are over our recommended invoice limits, find their
-                duplicates and replace it with a singular invoice. Uses Xero API
-                and complex data structures.
-              </p>
-              <figure id="blogDetailImg" class="image is-16by9">
-                <a
-                  href="https://github.com/leslie-alldridge/xero-duplicate-invoice-reducer"
-                  target="_blank"
-                >
-                  <img id="round" src="/duplicate.png" />
-                </a>
-              </figure>
-            </article>
-          </div>
-          <div id="firstCar" class="tile">
-            <article class="tile is-child notification is-info">
-              <p class="title">Rubik's Cube Timer</p>
-              <p class="subtitle">
-                Includes a database with graphs, historical times, sessions and
-                bests (mobile first). Consumes external API's.
-              </p>
-              <figure id="blogDetailImg" class="image is-16by9">
-                <a href="https://cubetimer102.herokuapp.com/" target="_blank">
-                  <img id="round" src="/cube.png" />
-                </a>
-              </figure>
-            </article>
-          </div>
-          <div id="firstCar" class="tile">
-            <article class="tile is-child notification is-info">
-              <p class="title">Motivation Slack Bot</p>
-              <p class="subtitle">
-                Provides users access to motivational videos, images and quotes.
-                The ability to search by topic is also available. Currently
-                added to Xero's workspace and widely used by the team.
-              </p>
-              <figure id="blogDetailImg" class="image is-16by9">
-                <a
-                  href="https://github.com/leslie-alldridge/slackbot-motivation"
-                  target="_blank"
-                >
-                  <img id="round" src="/motivate.png" />
-                </a>
-              </figure>
-            </article>
-          </div>
-        </Carousel>
-      </div>
+            </Thumbnail>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
